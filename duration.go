d9d9193f4a5d10d6a4738a2861cb97211b942869
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2023-06-02 17:13:31 +0200

    Implements the Duration type

---
+++ duration.go
@@ -0,0 +1,67 @@
+package humanize
+
+import (
+	"fmt"
+	"time"
+)
+
+// A Duration is a time.Duration that is more readable for long
+// time. i.e. 103h03m15s will format to 4 days 7 hours
+type Duration time.Duration
+
+// Missing time package constants for larger times
+const (
+	Day   = 24 * time.Hour // a day
+	Week  = 7 * Day        // a Week
+	Month = 30 * Day       // a Month
+	Year  = 365 * Day      // a Year
+)
+
+// String implements fmt.Stringer interface
+func (d Duration) String() string {
+	dur := time.Duration(d)
+	switch {
+	case dur > Year:
+		dur = dur.Truncate(Day)
+		return formatDuration(dur, Year, Month, "year", "month")
+	case dur > Month:
+		dur = dur.Truncate(Day)
+		return formatDuration(dur, Month, Week, "month", "week")
+	case dur > Week:
+		dur = dur.Truncate(Day)
+		return formatDuration(dur, Week, Day, "week", "day")
+	case dur > Day:
+		dur = dur.Truncate(time.Hour)
+		return formatDuration(dur, Day, time.Hour, "day", "hour")
+	case dur > time.Hour:
+		dur = dur.Truncate(time.Minute)
+		return formatDuration(dur, time.Hour, time.Minute, "h", "m")
+	default:
+		return fmt.Sprintf("%s", dur)
+	}
+}
+
+func mayPluralize(units string, n int, onlyLeft bool) string {
+	if len(units) == 1 {
+		return units
+	}
+	if n > 1 {
+		units += "s"
+	}
+	if onlyLeft == true {
+		return " " + units
+	}
+	return " " + units + " "
+}
+
+func formatDuration(d, base, rem time.Duration, baseUnit, remUnit string) string {
+	trunc := d.Truncate(base)
+	remainder := int((d - trunc) / rem)
+	value := int(trunc / base)
+	if remainder <= 0 {
+		return fmt.Sprintf("%d%s", value, mayPluralize(baseUnit, value, true))
+	}
+	return fmt.Sprintf("%d%s%d%s",
+		value, mayPluralize(baseUnit, value, false),
+		remainder, mayPluralize(remUnit, remainder, true))
+}
---
+++ duration_test.go
@@ -0,0 +1,39 @@
+package humanize
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestDuration(t *testing.T) {
+	testdata := []struct {
+		Value    time.Duration
+		Expected string
+	}{
+		{Value: 0, Expected: "0s"},
+		{Value: 1023, Expected: "1.023µs"},
+		{Value: 67*time.Minute + 18*time.Second, Expected: "1h7m"},
+		{Value: 13*time.Hour + 58*time.Minute + 54*time.Second, Expected: "13h58m"},
+		{Value: 58*time.Hour + 12*time.Minute, Expected: "2 days 10 hours"},
+		{Value: 7*Day + 11*time.Hour, Expected: "1 week"},
+		{Value: 9*Day + 13*time.Hour, Expected: "1 week 2 days"},
+		{Value: 30*Day + 1*time.Hour, Expected: "1 month"},
+		{Value: 5*Month + 3*Week + 2*Day, Expected: "5 months 3 weeks"},
+		{Value: 12*Month + 1*Week + 2*Day, Expected: "1 year"},
+		{Value: 26*Month + 3*Week + 2*Day, Expected: "2 years 2 months"},
+	}
+
+	for _, d := range testdata {
+		formatted := fmt.Sprintf("%s", Duration(d.Value))
+		if formatted != d.Expected {
+			t.Errorf("Expected %s to format to %s, got: %s",
+				d.Value, d.Expected, formatted)
+		}
+	}
+}
+
+func ExampleDuration() {
+	fmt.Printf("%s\n", Duration(9*Day+45*time.Hour))
+	// Output: 1 week 3 days
+}
